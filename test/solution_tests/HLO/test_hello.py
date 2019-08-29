0134e9a508fc33e4d8f8e7c70412981cceccd9f5
sbx_user1051 <sbx_user1051@169.254.170.141>
2019-08-29 17:44:06 +0000

    Thu Aug 29 17:44:06 UTC 2019

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -1,5 +1,9 @@
 from solutions.HLO import hello_solution
+import pytest
 
 
-def test_hello():
-    assert hello_solution.hello("Hello") == hello_solution.ALWAYS_RETURN_STR
+possible_friends = [["John", "Hello, John!"],]
+
+@pytest.mark.parametrize("test_name,test_greeting", possible_friends)
+def test_hello(test_name, test_greeting):
+    assert hello_solution.hello(test_name) == test_greeting
