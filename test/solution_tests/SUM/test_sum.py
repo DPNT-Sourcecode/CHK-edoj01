60bb26022e38b8185966321c4e7d1ad4c1cb77cc
sbx_user1051 <sbx_user1051@169.254.170.141>
2019-08-29 17:28:24 +0000

    Thu Aug 29 17:28:24 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,7 +3,11 @@ from hypothesis import given
 from hypothesis.strategies import integers
 
 
-@given(x=integers(min_value=0, max_value=100), y=integers(min_value=0, max_value=100))
+@given(
+    x=integers(min_value=0, max_value=100),
+    y=integers(min_value=0, max_value=100)
+)
 def test_sum(x, y):
     assert sum_solution.compute(x, y) == x + y
+
 
