08b5c2cf464dfcdad3622caf4cd66046e65a1fbd
sbx_user1051 <sbx_user1051@169.254.10.213>
2019-08-29 18:24:54 +0000

    Thu Aug 29 18:24:54 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,32 +1,23 @@
-"""
-Our price table and offers: 
-+------+-------+----------------+
-| Item | Price | Special offers |
-+------+-------+----------------+
-| A    | 50    | 3A for 130     |
-| B    | 30    | 2B for 45      |
-| C    | 20    |                |
-| D    | 15    |                |
-+------+-------+----------------+
-"""
 from solutions.CHK import checkout_solution
-from hypothesis import given
-from hypothesis.strategies import integers
-
 
 
 def test_checkout():
-    assert checkout_solution.checkout("ABCD") == 4
+    assert checkout_solution.checkout("ABCD") == 115
+    assert checkout_solution.checkout("ABCDE") == 114
+
 
 
 def test_sum_of_sku():
-    assert checkout_solution.sum_of_sku(1,1,1,1) == 0
+    assert checkout_solution.sum_of_sku(1,1,1,1) == 1
 
 
 def test_calcATotal():
-    assert checkout_solution.calcATotal(1) == 0
+    assert checkout_solution.calcATotal(1) == 50
+    assert checkout_solution.calcATotal(7) == 310
 
 
 def test_calcBTotal():
-    assert checkout_solution.calcBTotal(1) == 0
+    assert checkout_solution.calcBTotal(1) == 30
+    assert checkout_solution.calcBTotal(13) == 300
+
 
