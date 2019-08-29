2b853da17f584ae03f0143ad4d71aa6c7746d9c2
sbx_user1051 <sbx_user1051@169.254.170.141>
2019-08-29 17:42:13 +0000

    Thu Aug 29 17:42:13 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,12 +1,15 @@
-ALWAYS_RETURN_STR= "Hello, World!" 
+ALWAYS_RETURN_TMP = "Hello, {name}!" 
+"""Constant template to expand the hello greeting
+"""
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str):
     """Some hello message that completely ignores the parameter.
 
-    :param friend_name: undefined
+    :param friend_name: the name of the friend to greet
     :type friend_name: string
-    :rtype: int
+    :rtype: str
     """
-    return ALWAYS_RETURN_STR
+    return ALWAYS_RETURN_TMP.format(name=friend_name)
+
