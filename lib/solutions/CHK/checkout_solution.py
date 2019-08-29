bbdf86e20c175b241deb4287b87df86b3f0fdd0b
sbx_user1051 <sbx_user1051@169.254.10.213>
2019-08-29 17:56:11 +0000

    Thu Aug 29 17:56:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,14 @@
+from typing import Optional
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> Optional[int]:
+    """Return -1 is sku is not a string.
+
+    :param skus: Product skus for checkout
+    :rtype: integer
+    """
+    if not isinstance(skus, str):
+        return -1
+
