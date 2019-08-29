8049bc9f9adc98a43c5657190ac2941e9e5e4de9
sbx_user1051 <sbx_user1051@169.254.10.213>
2019-08-29 18:11:11 +0000

    Thu Aug 29 18:11:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,19 +11,36 @@ Our price table and offers:
 +------+-------+----------------+
 """
 
-def sum_value_of_a(num_a:int) -> int:
-    specials, whole_price = divmod(num_a, 3)
-    return (specials * 130) + (whole_price * 50) 
 
 def sum_of_sku(
     num_items: int,
     price: int,
     special_vol: int=None,
     special_price: int=None
-):
-    specials, whole_price = divmod(num_items, special_vol)
-    return (specials * special_price) + (whole_price * price)
+) -> int:
+    """Calculate the total owed for the number of items of a given item
+    in the skus arg.
 
+    :param num_items: count of item
+    :param price: the whole item price of the individual items
+    :param special_vol: the volume required to qualify for discount
+    :param special_price: the price for a discount
+    :returns: total price for the items of SKU
+    :rtype integer:
+    """
+    if special_vol is not None:
+        specials, whole_price = divmod(num_items, special_vol)
+        return (specials * special_price) + (whole_price * price)
+
+
+def calcATotal(num_as: int) -> int:
+    """Return total owed for A SKU from volume"""
+    return sum_of_sku(num_as, 50, 3, 130)
+
+
+def calcBTotal(num_bs: int) -> int:
+    """Return total owed for B SKU from volume"""
+    return sum_of_sku(num_bs, 30, 2, 45)
 
 
 # noinspection PyUnusedLocal
@@ -34,8 +51,10 @@ def checkout(skus: str) -> Optional[int]:
     :param skus: Product skus for checkout
     :rtype: integer
     """
+    A_vol = 
 
     if not isinstance(skus, str):
         return -1
+
 
 
