444671c4efab8abe43a7510a4bb2e26db084e06d
sbx_user1051 <sbx_user1051@169.254.170.141>
2019-08-29 17:37:11 +0000

    Thu Aug 29 17:37:11 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,12 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str):
+    """Some hello message that completely ignores the parameter.
+
+    :param friend_name: undefined
+    :type friend_name: string
+    :rtype: int
+    """
+    return "Any String"
+
